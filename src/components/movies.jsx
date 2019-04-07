c1773f83c969c91d46cd5cb1375f4008a061fef0
Jean TOURE <touremjean@hotmail.com>
2019-04-07 23:54:40 +0000

    Initial commit

---
+++ src/components/movies.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  handleDelete = movie => {
+    this.setState({
+      movies: this.state.movies.filter(
+        currentMovie => currentMovie._id !== movie._id
+      )
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>{this.tableTitle()}</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => {
+              return (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <button
+                      onClick={() => this.handleDelete(movie)}
+                      className="btn btn-danger btn-sm"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  tableTitle() {
+    if (this.state.movies.length === 0)
+      return "There no movies in the datatbase.";
+    return `Showing ${this.state.movies.length} in the database.`;
+  }
+}
+
+export default Movies;
